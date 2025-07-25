28de27112513174b4e7cf667eff35c8f84e3bf36
ajaniadobe <ajani@adobe.com>
2025-07-25 12:57:37 -0400

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -128,6 +128,8 @@ async function loadLazy(doc) {
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
   loadFonts();
+
+  import('../tools/sidekick/aem-genai-variations.js');
 }
 
 /**
